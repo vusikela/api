24761cf56f18be2eeecf524a42a2d723123fdade
SATHEESH VUSIKELA <svusikela@teamsupport.com>
2024-01-19 14:55:13 +0200

    4th commit with local branch- am added the new line

--- src/test/java/com/rest/Test123.java
+++ src/test/java/com/rest/Test123.java
@@ -17,7 +17,8 @@ public class Test123 {
                      get("/workspaces").
                 then().
                     log().all().
-                    //assertThat().
+                    assertThat().
+                //testing
                     statusCode(200);
 
     }
